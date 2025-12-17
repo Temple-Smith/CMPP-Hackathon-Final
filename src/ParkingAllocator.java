3a8c0a45a9f72a83c447e7dad2f05ddc0a8739e4
Joanna <joannamarie.albento@gmail.com>
2025-12-17 12:41:14 -0700

    Update ParkingAllocator.java

--- src/ParkingAllocator.java
+++ src/ParkingAllocator.java
@@ -1,28 +1,32 @@
 import exceptions.ParkingOverflowException;
 import exceptions.SpotUnavailableException;
-import model.ParkingLot;
-import model.ParkingSpot;
-import model.Vehicle;
 
-public class ParkingAllocator{
-    private final ParkingLot parkingLot;
+import java.util.List;
+
+public class ParkingAllocator {
+    private final List<ParkingSpot> parkingLot;
     private final AvailabilityChecker availabilityChecker;
 
-    public ParkingAllocator(){
-        this.parkingLot = ParkingLot;
-        this.availabilityChecker = new AvailabilityChecker();
+    public ParkingAllocator(List<ParkingSpot> parkingLot) {
+        this.parkingLot = parkingLot;
+        this.availabilityChecker = new AvailabilityChecker(parkingLot);
+    }
 
-        public ParkingSpot assignSpot(Vehicle v) throws ParkingOverflowException, SpotUnavailableException{
-            ParkingSpot parkingSpot = availabilityChecker.findAvailableSpot(v);
-            if(parkingSpot == null){
+    public ParkingSpot assignSpot(Vehicle v) throws ParkingOverflowException, SpotUnavailableException {
+        for (ParkingSpot spot : parkingLot) {
+            if (v.getSize().equals(spot.getSize())) {
+                spot.occupySpot();
+            } else {
                 throw new ParkingOverflowException("Parking spot not available for this vehicle: "+ v);
             }
-            boolean successFullyOccupy = parkingSpot.occupySpot();
-            if(!successFullyOccupy){
-                throw new SpotUnavailableException("Parking Spot is occupied. Try again later.");
-            }
-            return parkingSpot;
         }
+
+        boolean successFullyOccupy = parkingSpot.isOccupied();
+
+        if(!successFullyOccupy){
+            throw new SpotUnavailableException("Parking Spot is occupied. Try again later.");
+        }
+        return parkingSpot;
     }
-    
+
 }
