fd017959ff3f3913418547ebbc51cb11c5718090
Drew Temple-Smith <drewtemplesmith@gmail.com>
2025-12-17 12:18:01 -0700

    Parking Lot changes

--- ParkingLot.java
+++ ParkingLot.java
@@ -7,6 +7,7 @@ public class ParkingLot {
     public ParkingLot(List<ParkingSpot> parkingSpots) {
         this.parkingSpots = parkingSpots;
     }
+    
     // Find an available parking spot for a given vehicle
     public ParkingSpot findAvailableSpot(Vehicle vehicle) {
         for (ParkingSpot spot : parkingSpots) {
